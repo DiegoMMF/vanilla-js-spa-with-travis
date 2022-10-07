638fd37b1d49e2d48f655c5110cb1c6d9de65aac
DiegoMMF <diegomaldinifreyre@gmail.com>
2022-10-07 17:42:28 -0300

    first commit
    has webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+
+
+export default {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: [ '.js' ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin([
+            {
+                inject: true,
+                template: '.public/index.html',
+                filename: './index.html'
+            }
+        ])
+    ]
+}
